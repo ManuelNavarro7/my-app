dfa1158ff926808472e9905201dddd1d00069874
ManuelNavarro7 <manuel.mnavarro@hotmail.com>
2021-11-26 11:56:40 -0300

    Prueba

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with coder
+          Learn React with Coder
         </a>
       </header>
     </div>
